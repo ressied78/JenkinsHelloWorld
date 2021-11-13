5922df718d6221ebff768c20eaca749a2c97cab4
RESSIE JANE DELA CRUZ <90600910+ressied78@users.noreply.github.com>
2021-11-13 23:35:35 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("*****~HELLO WORLD! HELLO JENKINS~*****");
+        System.out.println("*****~HELLO JENKINS! Be Good To Me~*****");
     }
 }
